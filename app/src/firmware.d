65415caea177baae9c02853ee46f414bc96bc801
chenzixuan <czx211355007@gmail.com>
2024-04-14 23:12:38 +0800

    add files

---
+++ app/src/firmware.d
@@ -0,0 +1,24 @@
+src/firmware.o: src/firmware.c \
+ ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ /usr/local/arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/local/arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /usr/local/arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /usr/local/arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ /usr/local/arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f4/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/f4/pwr.h \
+ ../libopencm3/include/libopencm3/stm32/common/pwr_common_v1.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/f4/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
